468c3fa253f2af53e923b9148f3747fc88ced99d
Andy Kreling <krelinga@gmail.com>
2025-06-13 17:37:22 +0000

    try calling to my service from a very simple binary.

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
-    fmt.Println("Hello World!")
+	fmt.Println("Hello World!")
+	resp, err := http.Get("https://tcserver.i.krel.ing/")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading body:", err)
+		return
+	}
+	fmt.Println(string(body))
 }
